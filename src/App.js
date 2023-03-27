38f2d43fe21677edd006409c291a5a10e3d95df1
mohi14 <mohiuddinkhan1409@gmail.com>
2023-03-27 10:23:32 +0600

    website language change done using i18n

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
 
 function App() {
+  const { t } = useTranslation();
+  const handleEnglish = () => {
+    i18next.changeLanguage("en");
+    localStorage.setItem("language", "en");
+  };
+  const handleJapanese = () => {
+    i18next.changeLanguage("ja");
+    localStorage.setItem("language", "ja");
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {t("first")} <code>src/App.js</code> {t("second")} {t("third")}
         </p>
         <a
           className="App-link"
@@ -18,6 +31,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <button style={{ backgroundColor: "red" }} onClick={handleEnglish}>
+        English
+      </button>
+      <button onClick={handleJapanese}>Japanese</button>
     </div>
   );
 }
